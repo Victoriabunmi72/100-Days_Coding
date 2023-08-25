5a639c2a18c55a36fdf76acbcd82b7c7a68c67bd
Victoriabunmi72 <bhummhie97@gmail.com>
2023-08-25 14:58:22 +0100

    basic.py

---
+++ explanation/basic.py
@@ -0,0 +1,20 @@
+#Writing Our First Code
+
+print(data)
+print("Hello World")
+#The text HELLO WORLD is bounded by quotation marks because it is a string or a group of character.
+
+print(50)
+print(1000)
+print(3.142)
+#This will print a few numbers. Each will call to "print" moves the output to new line.
+
+#printing multiple pieces of data in a single command, by separating it with "commas".
+print(50, 1000, 3.142, "Hello World")
+
+print("Hello" end="")
+print("World")
+print("Hello" end="")
+print("World")
+#This is for multiple print statements in the same line
+#The value of end is appended to the output and the next print will continue from here.
